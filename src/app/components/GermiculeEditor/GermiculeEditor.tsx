c1629f9731203899f76652ee51ed33230019e31a
Derwent McElhinney <derwent.m@thoughtworks.com>
2020-11-01 15:15:38 +1100

    ♻️ split GermiucleEditor into own file

---
+++ src/app/components/GermiculeEditor/GermiculeEditor.tsx
@@ -0,0 +1,43 @@
+/**
+ *
+ * GermiculeEditor
+ *
+ * an interactive JSON editor for connections
+ *
+ */
+import React from 'react';
+import ReactJson from 'react-json-view';
+
+interface Props {
+  src: object;
+  style?: object;
+  onUpdateSrc(params: { src: object }): void;
+}
+
+export class GermiculeEditor extends React.Component<Props> {
+  render() {
+    return (
+      <ReactJson
+        src={this.props.src}
+        displayObjectSize={false}
+        displayDataTypes={false}
+        onEdit={params => {
+          // console.log('onEdit', params);
+          this.setState({ src: params.updated_src });
+          this.props.onUpdateSrc({ src: params.updated_src });
+        }}
+        onAdd={params => {
+          // console.log('onAdd', params);
+          this.setState({ src: params.updated_src });
+          this.props.onUpdateSrc({ src: params.updated_src });
+        }}
+        onDelete={params => {
+          // console.log('onDelete', params);
+          this.setState({ src: params.updated_src });
+          this.props.onUpdateSrc({ src: params.updated_src });
+        }}
+        style={this.props.style}
+      />
+    );
+  }
+}
